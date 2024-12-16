2a3c58ea97dd94ebdd324c2b6141dda2f97c37dc
Muliminty <313195303@qq.com>
2024-12-16 07:35:22 +0000

    style :  删除注释代码

--- src/router/modules/dashboard.ts
+++ src/router/modules/dashboard.ts
@@ -3,13 +3,12 @@ import { DataAnalysis } from '@element-plus/icons-vue'; // 导入图标
 const dashboardRoutes: RouteRecordRaw[] = [
     {
         path: '/dashboard',
-        // name: 'Dashboard',
         component: () => import('@/views/Dashboard/Dashboard.vue'),
         props: { icon: DataAnalysis },  // 将图标作为组件传递
         meta: {
             requiresAuth: true,
             layout: 'default',
-        }, // 路由元信息
+        },
     },
 ];
 
--- src/router/modules/user.ts
+++ src/router/modules/user.ts
@@ -4,14 +4,12 @@ import { Edit, Link, User } from '@element-plus/icons-vue'; // 导入图标
 const userRoutes: RouteRecordRaw[] = [
     {
         path: '/users',
-        // name: 'UserList',
         component: () => import('@/views/User/UserList.vue'),
         props: { icon: User },  // 将图标作为组件传递
         meta: { layout: 'default' }, // 设置为 'empty' 布局
     },
     {
         path: '/users/:id',
-        // name: 'UserDetail',
         component: () => import('@/views/User/UserDetail.vue'),
         props: {
             hiddenMenu: true,
@@ -22,16 +20,13 @@ const userRoutes: RouteRecordRaw[] = [
     },
     {
         path: '/test',
-        // name: 'test',
         props: {
             icon: Link,  // 将图标作为组件传递
         },
         meta: { layout: 'default' }, // 设置为 'empty' 布局
-
         children: [
             {
                 path: '/test1',
-                // name: 'test1',
                 component: () => import('@/components/HelloWorld.vue'),
                 props: {
                     icon: Edit,  // 将图标作为组件传递
@@ -39,7 +34,6 @@ const userRoutes: RouteRecordRaw[] = [
             },
             {
                 path: '/test2',
-                // name: 'test2',
                 component: () => import('@/components/HelloWorld2.vue'),
                 props: {
                     icon: Edit,  // 将图标作为组件传递
