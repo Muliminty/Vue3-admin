ab58df2692779a340c9fe2ea572b1ccca4679391
Muliminty <313195303@qq.com>
2024-12-23 01:41:21 +0000

    feat: 菜单名称

--- src/router/modules/dashboard.ts
+++ src/router/modules/dashboard.ts
@@ -5,6 +5,7 @@ const dashboardRoutes: RouteRecordRaw[] = [
         path: '/dashboard',
         component: () => import('@/views/Dashboard/Dashboard.vue'),
         props: { icon: DataAnalysis },  // 将图标作为组件传递
+        name: 'dashboard',
         meta: {
             requiresAuth: true,
             layout: 'default',
--- src/router/modules/user.ts
+++ src/router/modules/user.ts
@@ -4,6 +4,7 @@ import { Edit, Link, User } from '@element-plus/icons-vue'; // 导入图标
 const userRoutes: RouteRecordRaw[] = [
     {
         path: '/users',
+        name: 'users',
         component: () => import('@/views/User/UserList.vue'),
         props: { icon: User },  // 将图标作为组件传递
         meta: { layout: 'default' }, // 设置为 'empty' 布局
@@ -23,10 +24,12 @@ const userRoutes: RouteRecordRaw[] = [
         props: {
             icon: Link,  // 将图标作为组件传递
         },
+        name: 'test',
         meta: { layout: 'default' }, // 设置为 'empty' 布局
         children: [
             {
                 path: '/test1',
+                name: 'test1',
                 component: () => import('@/components/HelloWorld.vue'),
                 props: {
                     icon: Edit,  // 将图标作为组件传递
@@ -34,6 +37,7 @@ const userRoutes: RouteRecordRaw[] = [
             },
             {
                 path: '/test2',
+                name: 'test2',
                 component: () => import('@/components/HelloWorld2.vue'),
                 props: {
                     icon: Edit,  // 将图标作为组件传递
